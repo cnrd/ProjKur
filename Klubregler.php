c463a1da70e41eaa1669d15e24707dccb59600ff
Susanne <suzze-t@hotmail.com>
2014-04-24 16:25:13 +0200

    tilfoejet klubregler

---
+++ Klubregler.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <title>Nørrebro Fighters</title>
+    </head>
+
+    <body> 
+
+    <font color="#798E89">
+    <H1>  Klubregler  </H1>
+    
+    <H3>  Ansvar      </H3>
+    <p> 
+        * Vi træner i lånte lokaler og forventer, at I passer godt på vores udstyr og lokalerne
+    </p>
+    <p>
+        * Informer din træner, hvis du ikke kommer til træning i en længere periode fx ferie
+    </p>
+    <p>
+        * Al sport, træning og evt. kampe sker på eget ansvar og efter gældende regler i foreningen - medlemmerne er ikke forsikret af Nørrebro Fighters
+    </p>
+    <p>
+        * Du (eller dine forældre) skal betale kontingent til tiden (senest den 1. i måneden) og I skal indbetale for en hel sæson.
+    </p>
+    <p>
+        * Hvis et medlem ønsker at holde en pause fra træningen over to måneder, kan kontingentet sættes i bero i perioden
+    </p>
+    <p>
+        * Pigeholdene træner i tildækkede og aflåste lokaler tirsdag og torsdag, hvor herre/drenge ikke er velkomne, så det er vigtigt, at I alle husker, at døren <u>skal</u> være låst.
+    </p>
+    
+    
+    <H3>  Din opførsel </H3>
+    <p>
+        * Du skal respektere og hjælpe alle dine kammerater og din træner
+    </p>
+    <p>
+        * Vi accepterer ikke vold, dårligt sprogbrug eller generende opførsel
+    </p>
+    <p>
+        * Du skal altid høre efter, hvad din træner siger
+    </p>
+    <p>
+        * Du skal rydde op efter timen (udstyr på plads, feje, luk vinduer og persionner op)
+    </p>
+    <p>
+        * Aflevér glemte sager til din træner
+    </p>
+    
+    
+    <H3>  Din træning  </H3>
+    <p>
+        * Ingen sko i træningslokalet!
+    </p>
+    <p>
+        * Du skal hilse, når du træder ind/ud af træningslokalet/dojo'en og hils på din træner
+    </p>
+    <p>
+        * Oplys dine trænere inden timen, hvis du har skader eller føler dig utilpas/sløj
+    </p>
+    <p>
+        * Du skal komme til tiden hver gang og være omklædt og klar ved times start
+    </p>
+    <p>
+        * Eleverne <u>skal</u> iføres beskyttelsesudstyr i direkte kamp
+    </p>
+    <p>
+        * Oplys din træner, hvis du ønsker at forlade lokalet (i tilfældet af at du bliver utilpas eller besvimer)
+    </p>
+    
+    
+    <H3>  Hygiejne     </H3>
+    <p>
+        * Vi opfordrer til god personlig hygiejne - Ta' hensyn til dine træningspartnere
+    </p>
+    <p>
+        * Anvend altid plaster til sår og rifter
+    </p>
+    <p>
+        * Du må ikke træne med bare fødder, hvis du har vorter
+    </p>
+    </font>
+    
+    <br><br>
+    <center>
+        <button type="Submit" value="yes">    Jeg accepterer         </button>
+        <button type="Submit" value="no">     Jeg accepterer ikke    </button>
+    </center>
+    </body>
+</html>
