02f71251b32c93bb38bf82d87762d7e6c7163532
Conrad Juhl Andersen <login@cnrd.me>
2014-05-08 14:09:50 +0200

    Added config file, changed DB login

---
+++ config.php
@@ -0,0 +1,11 @@
+<!DOCTYPE PHP>
+<?
+  class configClass{
+    public static $dbServer;
+    public static $dbLogin;
+    public static $dbPass;
+
+    $dbServer = "oci:dbname=//localhost:1521/dbwc";
+    $dbLogin  = "sqd917";
+    $dbPass   = "dbx2014"
+  }
