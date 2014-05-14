38cfecea86b5c66c7890c64d698b0bfc6d1b4aba
Conrad Juhl Andersen <login@cnrd.me>
2014-05-14 17:03:15 +0200

    Fixed up the sql statements

--- dbConfig.sql
+++ dbConfig.sql
@@ -1,51 +1,53 @@
-DROP TABLE MedlemsDatabase;
-DROP TABLE HoldDatabase;
-DROP TABLE LogInDatabase;
-DROP TABLE MereInfo;
-DROP TABLE KontaktPersonen;
-DROP TABLE Betaling;
-
-CREATE TABLE MedlemsDatabase(
+CREATE TABLE IF NOT EXISTS Website.MedlemsDatabase(
   fornavn VARCHAR(40) NOT NULL,
   efternavn VARCHAR(40) NOT NULL,
   medlemsemail VARCHAR(100) PRIMARY KEY,
-  telefon NUMBER(8,0) NOT NULL
+  telefon INT(8) NOT NULL
 );
-  
-CREATE TABLE HoldDatabase(
+
+CREATE TABLE IF NOT EXISTS Website.HoldDatabase(
   holdnavn VARCHAR(30),
   holdemail VARCHAR(100),
   startDato DATE,
   PRIMARY KEY(holdnavn,holdemail)
 );
 
-CREATE TABLE LogInDatabase(
+CREATE TABLE IF NOT EXISTS Website.LogInDatabase(
   logInEmail VARCHAR(100),
   password VARCHAR(100),
+  isAdmin INT(1),
   PRIMARY KEY(logInEmail,password)
 );
 
-CREATE TABLE MereInfo(
+CREATE TABLE IF NOT EXISTS Website.MereInfo(
   infoEmail VARCHAR(100) PRIMARY KEY,
   foedselsdato DATE NOT NULL,
   adresse VARCHAR(100) NOT NULL,
-  postnummer NUMBER(4,0) NOT NULL,
+  postnummer INT(4) NOT NULL,
   byen VARCHAR(50) NOT NULL,
   yderligereInfo VARCHAR(300),
   godkendt VARCHAR(3) DEFAULT 'nej'
 );
 
-CREATE TABLE KontaktPersonen(
+CREATE TABLE IF NOT EXISTS Website.KontaktPersonen(
   konNavn VARCHAR(70) NOT NULL,
-  konTelefon NUMBER(8,0) NOT NULL,
+  konTelefon INT(8) NOT NULL,
   konEmail VARCHAR(100) NOT NULL,
   konForEmail VARCHAR(100) PRIMARY KEY
 );
 
-CREATE TABLE Betaling(
+CREATE TABLE IF NOT EXISTS Website.Betaling(
   betalingsemail VARCHAR(100) PRIMARY KEY,
   status VARCHAR(3) DEFAULT 'nej',
   betalingsdato DATE
 );
 
-
+CREATE TABLE IF NOT EXISTS Website.BrugerHold(
+  fornavn VARCHAR(40) REFERENCES Website.MedlemsDatabase(fornavn),
+  efternavn VARCHAR(40) REFERENCES Website.MedlemsDatabase(efternavn),
+  holdnavn VARCHAR(30),
+  holdemail VARCHAR(100),
+  CONSTRAINT fk_column
+  FOREIGN KEY (holdnavn,holdemail) REFERENCES Website.HoldDatabase(holdnavn,holdemail),
+  PRIMARY KEY (fornavn,efternavn,holdnavn,holdemail)
+);
